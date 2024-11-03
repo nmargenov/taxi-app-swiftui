be622949471da13c6c809d1fa74f81ad8ccfdc97
nmargenov <nikolay_margenov@yahoo.com>
2024-11-03 22:11:49 +0200

    Initial project

---
+++ taxi-app-swiftui/taxi_app_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  taxi_app_swiftuiApp.swift
+//  taxi-app-swiftui
+//
+//  Created by Nikolai Margenov on 3.11.24.
+//
+
+import SwiftUI
+
+@main
+struct taxi_app_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
